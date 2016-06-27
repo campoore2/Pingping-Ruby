d482274e628bcdbd3c0fe0d6592dba460e9f809b
Cameron Poore and Matthew Giem <cameron@>
2016-06-27 11:43:41 -0700

    Working pingpong in Ruby.

--- lib/pingPong.rb
+++ lib/pingPong.rb
@@ -1,11 +1,18 @@
 class Fixnum
   define_method(:pingPong) do
-    self.%(3).eql?(0)
-  end
-  define_method(:pingPong)do
-    self.%(5).eql?(0)
-  end
-  define_method(:pingPong)do
-    self.%(15).eql?(0)
+    number_array=[]
+    numbers = (1..self)
+    numbers.each()  do |number|
+      if number.%(15) == 0
+        number_array.push('pingpong')
+      elsif number.%(5) == 0
+        number_array.push('pong')
+      elsif number.%(3) == 0
+        number_array.push('ping')
+      else
+        number_array.push(number)
+      end
+    end
+    number_array
   end
 end
--- spec/pingPong_spec.rb
+++ spec/pingPong_spec.rb
@@ -2,17 +2,16 @@ require("rspec")
 require("pingPong.rb")
 
 describe("Fixnum#pingPong") do
-  it("input a number divisible by 3 and return ping") do
-    expect((3).pingPong()).to(eq(true))
+  it("input a number and out put an array with that many elements") do
+    expect((2).pingPong()).to(eq([1,2]))
   end
-  it("input a number divisible by 5 and return pong") do
-    expect((5).pingPong()).to(eq(true))
+  it("input a number divisible by 15 and return pingpong")do
+    expect((15).pingPong()).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"]))
   end
-  it("input a number divisible by 15 and return pingpong") do
-    expect((5).pingPong()).to(eq(true))
+  it("input a number divisible by 5 and return pong")do
+    expect((5).pingPong()).to(eq([1, 2, "ping", 4, "pong"]))
   end
-  it("is false for a number not divisible by 3")do
-    expect((3).pingPong()).to(eq(false))
+  it("input a number divisible by 3 and return ping")do
+    expect((3).pingPong()).to(eq([1, 2,"ping"]))
   end
-
 end
